b5f0e33e2657a6aa229dc45615037d2ff14f2a83
Eduardo Lima <106208300+EduRKL@users.noreply.github.com>
2023-06-21 02:01:13 +0000

    Comment in

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,4 +1,8 @@
 //O(n^2)
+// This algorithm is not stable because an earlier element can jump after an element 
+// of the the same value during a swap, but the algorithm is in place as it only needs additional 
+// memory to store the index to the minimum element.
+
 class Solution {
     public static List<Integer> sortList(List<Integer> unsortedList) {
         int n = unsortedList.size();
